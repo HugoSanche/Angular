ccb09e9d8ecf656291f837057b974349e7913787
HugoSanche <hbmclaren@gmail.com>
2023-05-27 17:46:58 -0600

    Work with my first proyect in Angular framework

---
+++ sales-proyect/src/app/sales-person-list/sales-person-list.component.ts
@@ -0,0 +1,24 @@
+import { Component,OnInit } from '@angular/core';
+import { SalesPerson } from './sales-person';
+
+@Component({
+  selector: 'app-sales-person-list',
+  templateUrl: './sales-person-list.component.html',
+  styleUrls: ['./sales-person-list.component.css']
+})
+export class SalesPersonListComponent implements OnInit{
+  
+  //create an array of objects
+  salesPersonList:SalesPerson[]=[
+    new SalesPerson("Veronica","perez","vero@hotmail.com",5000),
+    new SalesPerson("Arely","Vite","arely.vite@hotmail.com",3500),
+    new SalesPerson("Miriam","Rosario","rosario@hotmail.com",9500),
+    new SalesPerson("Monica","Ponce","vero@hotmail.com",2000),
+  ];
+constructor(){}
+
+  ngOnInit(): void {
+    
+  }
+
+}
